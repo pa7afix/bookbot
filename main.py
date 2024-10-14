7ae272c75d2c640ce9939c67d075f0c306edf5f6
Patrick Schaefer <patrick.schaefer@uni-due.de>
2024-10-14 22:22:22 +0200

    added functionality for counting occurrences

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-#        print(file_contents)
     count_words(file_contents)
+    count_number_of_letters(file_contents)
+
 
 def count_words(text):
     words = text.split()
     count = 0
     for word in words:
         count += 1
-    print(count)
+    print(f"{count} words are found in the document")
+
+def count_number_of_letters(text):
+    lowercase = text.lower()
+    letter_occurrence = {}
+    for letter in lowercase:
+        if letter in letter_occurrence:
+            letter_occurrence[letter] += 1
+        else:
+            letter_occurrence[letter] = 1
+    print(letter_occurrence)
+
 
 main()
