31c1b20d6384f7b30cf31d9be7be628bf8866a8a
Patrick Schaefer <patrick.schaefer@uni-due.de>
2025-04-29 15:45:04 +0200

    moved statistical functions to stats.py

--- main.py
+++ main.py
@@ -1,60 +1,14 @@
+from stats import count_words
+from stats import count_number_of_letters
+from stats import created_culled_sorted_list
+from stats import print_frequency_report
+
 def main():
     print(f"{count_words(file_contents)} words found in the document")
     print_frequency_report(file_contents)
-
-
+    
 # imports the book
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
-
-#counts the number of words
-def count_words(text):
-    words = text.split()
-    count = 0
-    for word in words:
-        count += 1
-    return count
-
-# counts the occurrence of symbols
-def count_number_of_letters(text):
-    lowercase = text.lower()
-    letter_occurrence = {}
-    for letter in lowercase:
-        if letter in letter_occurrence:
-            letter_occurrence[letter] += 1
-        else:
-            letter_occurrence[letter] = 1
-    return letter_occurrence
-
-# organizes a sorted list with dictionaries as its elements which contain the frequency of letters encoded in a format
-# which allows for easy access
-def created_culled_sorted_list(text):
-    culled_list = []
-    dictionary = {}
-    lowercase = text.lower()
-    split_text = lowercase.split()
-    for word in split_text:
-        for letter in word:
-            if letter.isalpha():
-                if letter in dictionary:
-                    dictionary[letter] += 1
-                else:
-                    dictionary[letter] = 1
-    for letter in dictionary:
-        dummy_dict = {"character": letter, "frequency": dictionary[letter]}
-        culled_list.append(dummy_dict)
-    culled_list.sort(reverse=True, key=lambda x: x["frequency"])
-    return culled_list
-
-# prints a report on how often each letter appears within the text, sorted by its frequency
-def print_frequency_report(text):
-    culled_list = created_culled_sorted_list(text)
-    count = count_words(text)
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{count} words found in the document")
-    for letter in culled_list:
-        print(f"The '{letter['character']}' character was found '{letter['frequency']}' times")
-    print("--- End report---")
-
 
 main()
---
+++ stats.py
@@ -0,0 +1,48 @@
+#counts the number of words
+def count_words(text):
+    words = text.split()
+    count = 0
+    for word in words:
+        count += 1
+    return count
+
+# counts the occurrence of symbols
+def count_number_of_letters(text):
+    lowercase = text.lower()
+    letter_occurrence = {}
+    for letter in lowercase:
+        if letter in letter_occurrence:
+            letter_occurrence[letter] += 1
+        else:
+            letter_occurrence[letter] = 1
+    return letter_occurrence
+
+# organizes a sorted list with dictionaries as its elements which contain the frequency of letters encoded in a format
+# which allows for easy access
+def created_culled_sorted_list(text):
+    culled_list = []
+    dictionary = {}
+    lowercase = text.lower()
+    split_text = lowercase.split()
+    for word in split_text:
+        for letter in word:
+            if letter.isalpha():
+                if letter in dictionary:
+                    dictionary[letter] += 1
+                else:
+                    dictionary[letter] = 1
+    for letter in dictionary:
+        dummy_dict = {"character": letter, "frequency": dictionary[letter]}
+        culled_list.append(dummy_dict)
+    culled_list.sort(reverse=True, key=lambda x: x["frequency"])
+    return culled_list
+
+# prints a report on how often each letter appears within the text, sorted by its frequency
+def print_frequency_report(text):
+    culled_list = created_culled_sorted_list(text)
+    count = count_words(text)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document")
+    for letter in culled_list:
+        print(f"The '{letter['character']}' character was found '{letter['frequency']}' times")
+    print("--- End report---")
